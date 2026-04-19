a1037cdd745300f5f4866d5740cf7939291c942d
Oleksiy Boyko <3101312@gmail.com>
2026-04-19 23:28:57 +0300

    update sql queries in sh_sql_try.sql

--- sh_sql_try.sql
+++ sh_sql_try.sql
@@ -65,38 +65,95 @@ ORDER BY
     country_name;
 
 
--- BOO
 
-SELECT * FROM sales s ;
 
-SELECT * FROM  products p ;
 
-SELECT * FROM  customers c ;
 
-SELECT * FROM times t ;
 
-SELECT * FROM channels c ;
 
-SELECT * FROM countries c ;
+
+
+
+
+
 
 
 -- Aggregate Function: Reporting
 
 -- For each product category, find the region in which it had maximum sales
 
-SELECT    prod_category,
-          country_region,
-          SUM(s.amount_sold) AS sales
-FROM      sh.sales s
-JOIN      sh.products p ON p.prod_id = s.prod_id
-JOIN      sh.customers cust ON cust.cust_id = s.cust_id
-JOIN      sh.channels ch ON ch.channel_id = s.channel_id
-JOIN      sh.countries cn ON cn.country_id = cust.country_id
-WHERE     time_id = TO_DATE('11-OCT-2001', 'DD-MON-YYYY')
-GROUP BY  prod_category,
-          country_region
-;
+SELECT
+    prod_category,
+    country_region,
+    SUM(s.amount_sold) AS sales,
+    MAX(SUM(amount_sold)) OVER (PARTITION BY p.prod_category) AS max_reg_sales
+FROM
+    sh.sales s
+JOIN
+    sh.products p ON p.prod_id = s.prod_id -- to get prod_category
+JOIN
+    sh.customers cust ON cust.cust_id = s.cust_id -- to get country_id of customer
+--JOIN
+    --sh.channels ch ON ch.channel_id = s.channel_id -- present but unused in SELECT
+JOIN
+    sh.countries cn ON cn.country_id = cust.country_id -- to get country_region
+WHERE
+    time_id = TO_DATE('11-OCT-2001', 'DD-MON-YYYY')
+GROUP BY
+    prod_category,
+    country_region;
 
+
+-- BOO
+
+SELECT prod_category, country_region, sales
+FROM (
+    SELECT
+        p.prod_category,
+        cn.country_region,
+        SUM(s.amount_sold) AS sales,
+        MAX(SUM(s.amount_sold)) OVER (PARTITION BY p.prod_category) AS max_reg_sales
+    FROM sh.sales s
+    JOIN sh.products  p    ON p.prod_id     = s.prod_id
+    JOIN sh.customers cust ON cust.cust_id  = s.cust_id
+    JOIN sh.countries cn   ON cn.country_id = cust.country_id
+    WHERE s.time_id = TO_DATE('11-OCT-2001', 'DD-MON-YYYY')
+    GROUP BY p.prod_category, cn.country_region
+) t
+WHERE sales = max_reg_sales;
+
+
+-- TRY
+
+
+SELECT
+    prod_category,
+    country_region,
+    sales
+FROM (
+    SELECT
+        prod_category,
+        country_region,
+        SUM(s.amount_sold) AS sales,
+        MAX(SUM(amount_sold)) OVER (PARTITION BY p.prod_category) AS max_reg_sales
+    FROM
+        sh.sales s
+    JOIN
+        sh.products p ON p.prod_id =s.prod_id
+    JOIN
+        sh.customers cust ON cust.cust_id = s.cust_id
+    JOIN
+        sh.channels ch ON ch.channel_id = s.channel_id
+    JOIN
+        sh.countries cn ON cn.country_id = cust.country_id
+    WHERE
+        time_id = TO_DATE('11-OCT-2001', 'DD-MON-YYYY')
+    GROUP BY
+        prod_category,
+        country_region
+) tab
+WHERE
+    sales = max_reg_sales;
 
 
 
